e42469ba95f20ee1050c561087649b34b90e4a0e
Sharjeel Ahmad <sharjeel_ahmad@manulife.com>
2025-10-18 18:54:48 -0400

    Initial Project.

---
+++ SkyLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SkyLens
+//
+//  Created by Sharjeel Ahmad on 2025-10-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SkyLens/SkyLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyLensApp.swift
+//  SkyLens
+//
+//  Created by Sharjeel Ahmad on 2025-10-18.
+//
+
+import SwiftUI
+
+@main
+struct SkyLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
